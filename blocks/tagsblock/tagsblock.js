4b2646e40fa5ac793ba1f53b6738f7f59ecaa4cc
prakharpathak <prakhar.pathak@nagarro.com>
2023-08-28 18:07:39 +0530

    tagsblock suggested fatures

--- blocks/tagsblock/tagsblock.js
+++ blocks/tagsblock/tagsblock.js
@@ -4,17 +4,25 @@ export default function decorate(block) {
         const propertydiv = document.createElement('div');
         propertydiv.classList.add(`tagsblock-`+row.children[0].innerHTML);
 
-        const redirectList = document.createElement('ul');
-        redirectList.classList.add(row.children[0].innerHTML+'-list');
+        if(row.children[0].innerHTML === "title"){
+            const propertydiv = document.createElement('div');
+            propertydiv.classList.add(`tagsblock-`+row.children[0].innerHTML);
+            propertydiv.innerHTML = `<p> ${row.children[1].innerHTML} </p>`;
+            blockAuthor.appendChild(propertydiv);
+        }else{
+            const redirectList = document.createElement('ul');
+            redirectList.classList.add(row.children[0].innerHTML+'-list');
 
-        const redirectL = row.children[1].children;
-        for(var link of redirectL){
-            const li = document.createElement('li');
-            li.innerHTML = link.innerHTML;
-            redirectList.appendChild(li);
+            const redirectL = row.children[1].children;
+            for(var link of redirectL){
+                const li = document.createElement('li');
+                li.innerHTML = link.innerHTML;
+                redirectList.appendChild(li);
+            }
+            propertydiv.appendChild(redirectList);
+            blockAuthor.appendChild(propertydiv);
         }
-        propertydiv.appendChild(redirectList);
-        blockAuthor.appendChild(propertydiv);
+        
     });
     block.innerHTML = blockAuthor.innerHTML;
 }
