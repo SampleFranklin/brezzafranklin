09081d4bd116100d17b7cbdce8a7a1d04fa6ed2a
prakharpathak <prakhar.pathak@nagarro.com>
2023-08-29 11:56:33 +0530

    carousel bug slick

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -3,26 +3,26 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 $(document).ready(function(){
   $('.multiple-items').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 1,
     arrows:true
   })
 
   $('.stories').slick({
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     arrows:true
-  })
+  }).slick('refresh')
 });
 
 export default function decorate(block) {
-    block.classList.add('multiple-items');
-    [...block.children].forEach((row, i) => {
-      const classes = ['image', 'text'];
-      classes.forEach((e, j) => {
-        row.children[j].classList.add(`carousel-${e}`);
-      });
+  block.classList.add('multiple-items');
+  [...block.children].forEach((row, i) => {
+    const classes = ['image', 'text'];
+    classes.forEach((e, j) => {
+      row.children[j].classList.add(`carousel-${e}`);
     });
-    console.log(block);
-  }
+  });
+  console.log(block);
+}
