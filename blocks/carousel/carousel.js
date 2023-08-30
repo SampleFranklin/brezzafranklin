04d0091f5ba0314e09ef46178071c7e7e6dbc786
prakharpathak <prakhar.pathak@nagarro.com>
2023-08-30 16:52:49 +0530

    swiper buttons

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,34 +1,4 @@
-/*   $(".multiple-items").slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-  });
-
-  $(".stories")
-    .slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      arrows: true,
-    })
-    .slick("refresh");
-
-  setTimeout(() => {
-    $(".stories").slick("refresh");
-  }, 100);*/
-
 export default function decorate(block) {
-  // block.classList.add("multiple-items");
-
-  // [...block.children].forEach((row, i) => {
-  //   console.log(row);
-  //   const classes = ["image", "text"];
-  //   classes.forEach((e, j) => {
-  //     row.children[j].classList.add(`carousel-${e}`);
-  //   });
-  // });
-
   block.classList.add("swiper-wrapper");
   [...block.children].forEach((row, i) => {
     row.classList.add('swiper-slide');
@@ -39,33 +9,30 @@ export default function decorate(block) {
     });
   });
 
+  const previousButton = document.createElement('div');
+  const nextButton = document.createElement('div');
+  previousButton.classList.add("swiper-button-prev");
+  nextButton.classList.add("swiper-button-next");
+
+  if(block.classList.contains("stories")){
+    previousButton.classList.add("stories-prev");
+    nextButton.classList.add("stories-next");
+  }
+
+  block.parentElement.appendChild(previousButton);
+  block.parentElement.appendChild(nextButton);
+
   const swiper = new Swiper('.carousel-wrapper', {
-    effect: 'coverflow',
-    coverflowEffect: {
-      rotate: 30,
-      slideShadows: false,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
+  //   breakpoints: {
+  //     391: {
+  //         slidesPerView: 1
+  //     }
+  // }
   });
-  console.log("hello");
 }
 
-// $(document).ready(function () {
-//   $(".multiple-items").slick({
-//     infinite: false,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     arrows: true,
-//     variableWidth: true,
-//     responsive: [
-//       {
-//         breakpoint: 391,
-//         settings: {
-//           arrows: true,
-//           slidesToShow: 1,
-//           variableWidth: false,
-//           slidesToScroll: 3,
-//         },
-//       },
-//     ],
-//   });
-// });
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,10 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+// var script1 = document.createElement('script');
+// script1.src = 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+// document.getElementsByTagName('head')[0].appendChild(script1);
+
+// var script1 = document.createElement('script');
+// script1.src = "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js";
+// document.getElementsByTagName('head')[0].appendChild(script1);
