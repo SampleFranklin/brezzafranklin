dcd119e981772ab73f3d12bbe9e57ca5edda37a1
prakharpathak <prakhar.pathak@nagarro.com>
2023-08-30 12:41:59 +0530

    swiper changes

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -19,33 +19,53 @@
   }, 100);*/
 
 export default function decorate(block) {
-  block.classList.add("multiple-items");
+  // block.classList.add("multiple-items");
 
+  // [...block.children].forEach((row, i) => {
+  //   console.log(row);
+  //   const classes = ["image", "text"];
+  //   classes.forEach((e, j) => {
+  //     row.children[j].classList.add(`carousel-${e}`);
+  //   });
+  // });
+
+  block.classList.add("swiper-wrapper");
   [...block.children].forEach((row, i) => {
-    console.log(row);
+    row.classList.add('swiper-slide');
     const classes = ["image", "text"];
+
     classes.forEach((e, j) => {
       row.children[j].classList.add(`carousel-${e}`);
     });
   });
-}
-$(document).ready(function () {
-  $(".multiple-items").slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 391,
-        settings: {
-          arrows: true,
-          slidesToShow: 1,
-          variableWidth: false,
-          slidesToScroll: 3,
-        },
-      },
-    ],
+
+  const swiper = new Swiper('.carousel-wrapper', {
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
   });
-});
+  console.log("hello");
+}
+
+// $(document).ready(function () {
+//   $(".multiple-items").slick({
+//     infinite: false,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     arrows: true,
+//     variableWidth: true,
+//     responsive: [
+//       {
+//         breakpoint: 391,
+//         settings: {
+//           arrows: true,
+//           slidesToShow: 1,
+//           variableWidth: false,
+//           slidesToScroll: 3,
+//         },
+//       },
+//     ],
+//   });
+// });
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,3 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-var script1 = document.createElement('script');
-script1.src = 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
-document.getElementsByTagName('head')[0].appendChild(script1);
