bd3e3f24a0ffe6baac0963b52016eccde4397ed6
prakharpathak <prakhar.pathak@nagarro.com>
2023-09-06 16:58:03 +0530

    footerbar added

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -19,7 +19,6 @@ export default async function decorate(block) {
     const footer = document.createElement('div');    
     footer.innerHTML = html;
     
-
     // Editing FooterColumns DIV
     const footerColDiv = footer.querySelector('.footercolumn');
     const newDiv = document.createElement('div');
@@ -77,8 +76,7 @@ export default async function decorate(block) {
     const footnoteDiv = footer.querySelector('.footnote > div > div');
     const footnoteList = footer.querySelector('.footnote > div > div > ul');
     footnoteDiv.classList.add('footnote')
-    footnoteList.classList.add('footnote-list')
-    console.log(footnoteDiv);
+    footnoteList.classList.add('footnote-list');
 
     [...footnoteList.children].forEach((row) => {
       const p = document.createElement('p');
@@ -93,7 +91,48 @@ export default async function decorate(block) {
     footerColDiv.append(socialCol);
     footerColDiv.append(footnoteDiv);
 
-    block.innerHTML = footerColDiv.innerHTML;
+    // without wrapper
+    // block.innerHTML = footerColDiv.innerHTML;
+
+    // with wrapper
+    const footerContent = document.createElement('div');
+    footerContent.classList.add("footer-content");
+    footerContent.innerHTML = footerColDiv.innerHTML;
+
+    // Footer Bar
+    const footerBar = footer.querySelector('.footerbar');
+    const footBarList = footer.querySelector('.footerbar > div > div > ul');
+
+    const footerBarDiv = document.createElement('div');
+    footerBarDiv.classList.add('footer-bar');
+
+    [...footerBar.children].forEach((row) => {
+      if(row.children[1].children.length === 0){
+        const propertydiv = document.createElement('div');
+        propertydiv.classList.add(row.children[0].innerHTML.replace(/\s+/g, "-").toLowerCase());
+        propertydiv.innerHTML = `<p> ${row.children[1].innerHTML} </p>`;
+        footerBarDiv.appendChild(propertydiv);
+      }else{
+        const propertydiv = document.createElement('div');
+        propertydiv.classList.add(row.children[0].innerHTML.replace(/\s+/g, "-").toLowerCase());
+
+        const listDiv = document.createElement('ul');
+        listDiv.classList.add('footerbar-list');
+
+        const links = footBarList.children;
+        for(var link of links){
+          const li = document.createElement('li');
+          li.innerHTML = link.innerHTML;
+          listDiv.appendChild(li);
+        }
+
+        propertydiv.appendChild(listDiv);
+        footerBarDiv.appendChild(propertydiv);
+      }
+    })
+    block.appendChild(footerContent);
+    block.appendChild(footerBarDiv);
+    // block.appendChild(footerBar);
     decorateIcons(footer);
   }
 }
