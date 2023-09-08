50501ba351e1cad2ddde98d04bfeda71c699753b
prakharpathak <prakhar.pathak@nagarro.com>
2023-09-08 16:16:21 +0530

    footer mobile

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,33 +5,25 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 function executeAccordion(){
+    const mediaQuery = window.matchMedia('(max-width: 980px)');
     const accordionSections = document.querySelectorAll('.col-accordion');
     accordionSections.forEach((section) => {
       const heading = section.querySelector('.col-list-heading');
       const content = section.querySelector('.col-list-content');
         
-      heading.addEventListener('click', () => {
-        console.log('clicked');
-        console.log(content.style.display !== 'block');
-        content.style.display = content.style.display !== 'block' ? 'block' : 'none';
-      });
+      if(mediaQuery.matches){
+        heading.addEventListener('click', () => {
+            console.log('clicked');
+            console.log(content.style.display !== 'block');
+            content.style.display = content.style.display !== 'block' ? 'block' : 'none';
+        });
+      }else{
+        heading.removeEventListener('click',()=>{});
+      }
+      
     });
 }
 
-
-function handleScreenSizeChange(mediaQuery) {
-    if (mediaQuery.matches) {
-      // Screen size is less than or equal to 980px
-      executeAccordion();
-    }
-}
-
-
-const mediaQuery = window.matchMedia('(max-width: 980px)');
-
-mediaQuery.addEventListener('change', (event) => {
-    if(mediaQuery.matches){
-        handleScreenSizeChange(event.target);
-    }
-  });
-handleScreenSizeChange(mediaQuery);
+window.addEventListener('resize', (event) => {
+    executeAccordion();
+});
