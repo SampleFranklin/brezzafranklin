569371827a0eee904b473fffec5ee681de7bb171
prakharpathak <prakhar.pathak@nagarro.com>
2023-09-11 13:53:16 +0530

    footer 11-09

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,25 +5,33 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 function executeAccordion(){
-    const mediaQuery = window.matchMedia('(max-width: 980px)');
+//     content.style.display = 'block';
+//     if(mediaQuery.matches){
+//         content.style.display = 'none';      
+//         heading.addEventListener('click', () => {
+//             content.style.display = content.style.display !== 'block' ? 'block' : 'none';
+//         });
+//     }else{
+//         console.log("Here")
+//         heading.replaceWith(heading.cloneNode(true));
+//     }
+// });
+    const mediaQuery = window.matchMedia('(max-width: 1023px)');
     const accordionSections = document.querySelectorAll('.col-accordion');
     accordionSections.forEach((section) => {
-      const heading = section.querySelector('.col-list-heading');
-      const content = section.querySelector('.col-list-content');
-        
-      if(mediaQuery.matches){
+    const heading = section.querySelector('.col-list-heading');
+    const content = section.querySelector('.col-list-content');
+      if(mediaQuery.matches ){
         heading.addEventListener('click', () => {
-            console.log('clicked');
-            console.log(content.style.display !== 'block');
             content.style.display = content.style.display !== 'block' ? 'block' : 'none';
         });
-      }else{
-        heading.removeEventListener('click',()=>{});
       }
-      
     });
 }
 
+
 window.addEventListener('resize', (event) => {
+    window.location.reload();
     executeAccordion();
 });
+executeAccordion();
