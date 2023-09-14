a569b6efb842cf568e7577b691040964280185f8
jhansi mythry <mythry.jhansi@nagarro.com>
2023-09-14 11:18:58 +0530

    footer accordian issue ifixed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,47 +4,48 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-function executeAccordion(){
-//     content.style.display = 'block';
-//     if(mediaQuery.matches){
-//         content.style.display = 'none';      
-//         heading.addEventListener('click', () => {
-//             content.style.display = content.style.display !== 'block' ? 'block' : 'none';
-//         });
-//     }else{
-//         console.log("Here")
-//         heading.replaceWith(heading.cloneNode(true));
-//     }
-// });
-    const mediaQuery = window.matchMedia('(max-width: 1023px)');
-    const accordionSections = document.querySelectorAll('.col-accordion');
-    accordionSections.forEach((section,index) => {
+function executeAccordion() {
+  //     content.style.display = 'block';
+  //     if(mediaQuery.matches){
+  //         content.style.display = 'none';      
+  //         heading.addEventListener('click', () => {
+  //             content.style.display = content.style.display !== 'block' ? 'block' : 'none';
+  //         });
+  //     }else{
+  //         console.log("Here")
+  //         heading.replaceWith(heading.cloneNode(true));
+  //     }
+  // });
+  const mediaQuery = window.matchMedia('(max-width: 1023px)');
+  const accordionSections = document.querySelectorAll('.col-accordion');
+  accordionSections.forEach((section, index) => {
     const heading = section.querySelector('.col-list-heading');
     const content = section.querySelector('.col-list-content');
-      if(mediaQuery.matches && index !== 0){
-        content.style.display = 'none';
-        if(heading.getAttribute("init") !== "true"){
-          heading.addEventListener('click', () => {
-            content.style.display = content.style.display !== 'block' ? 'block' : 'none';
-          });
-          heading.setAttribute("init","true");
-        }
-      }else{
-        content.style.display = 'block';
+    if (mediaQuery.matches && index !== 0) {
+      content.style.display = 'none';
+      if (heading.getAttribute("init") !== "true") {
+        heading.addEventListener('click', () => {
+          heading.classList.toggle('active');
+          content.style.display = content.style.display !== 'block' ? 'block' : 'none';
+        });
+        heading.setAttribute("init", "true");
       }
-    });
+    } else {
+      content.style.display = 'block';
+    }
+  });
 }
 
 
 const tagList = document.getElementById("tags-list");
 if (tagList) {
-    tagList.addEventListener("click", function (e) {
-        if (e.target.nodeName === 'LI') {
-            e.target.classList.toggle("bg-blue");
-        }
-    });
-} 
+  tagList.addEventListener("click", function (e) {
+    if (e.target.nodeName === 'LI') {
+      e.target.classList.toggle("bg-blue");
+    }
+  });
+}
 window.addEventListener('resize', (event) => {
-    executeAccordion();
+  executeAccordion();
 });
 executeAccordion();
